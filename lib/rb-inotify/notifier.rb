f731f06a53fd2d77012b8bb7240ff3323e18cf6d
Nathan Weizenbaum <nex342@gmail.com>
2010-04-25 01:31:41 -0700

    Properly fire recursive events when the :all_events flag is used.
    Closes gh-3

--- lib/rb-inotify/notifier.rb
+++ lib/rb-inotify/notifier.rb
@@ -179,7 +179,7 @@ module INotify
 
       rec_flags = [:create, :moved_to]
       return watch(path, *((flags - [:recursive]) | rec_flags)) do |event|
-        callback.call(event) unless (flags & event.flags).empty?
+        callback.call(event) if flags.include?(:all_events) || !(flags & event.flags).empty?
         next if (rec_flags & event.flags).empty? || !event.flags.include?(:isdir)
         watch(event.absolute_name, *flags, &callback)
       end
