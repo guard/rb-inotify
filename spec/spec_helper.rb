82f19d0d38c3364e9d392c84beebf5bc387f942f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-06-17 21:37:49 +1200

    Remove stuff that doesn't work on older rubies.
    * There are no tests and probably never will be.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,4 @@
 
-if ENV['COVERAGE'] || ENV['TRAVIS']
-	begin
-		require 'simplecov'
-		
-		SimpleCov.start do
-			add_filter "/spec/"
-		end
-		
-		if ENV['TRAVIS']
-			require 'coveralls'
-			Coveralls.wear!
-		end
-	rescue LoadError
-		warn "Could not load simplecov: #{$!}"
-	end
-end
-
 require "bundler/setup"
 require "rb-inotify"
 
